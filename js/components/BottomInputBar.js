30735528ac04cd68bab6af0abde97d337f1aa693
Steven Fang <fondoger@outlook.com>
2018-08-21 14:42:07 +0800

    format code

--- js/components/BottomInputBar.js
+++ js/components/BottomInputBar.js
@@ -35,106 +35,108 @@ export default class BottomInputBar extends React.Component {
   _onSendPress() {
     if (this.state.text === '' || !this.props.onSendPress)
       return;
-    this.setState({send_ing: true});
-    this.props.onSendPress(this.state.textValue, (shouldClean)=>{
-      this.setState({send_ing: false});
-      shouldClean ? this.setState({textValue: ''}): null;
+    this.setState({ send_ing: true });
+    this.props.onSendPress(this.state.textValue, (shouldClean) => {
+      this.setState({ send_ing: false });
+      shouldClean ? this.setState({ textValue: '' }) : null;
     });
   }
 
   _onChangeText(text) {
-    this.setState({textValue:text});
+    this.setState({ textValue: text });
   }
 
   _onEmojiSwitchPress() {
     if (this.state.showEmojiInput) {
-      this.setState({showEmojiInput: false});
+      this.setState({ showEmojiInput: false });
       this.textInput.focus();
     } else {
       Keyboard.dismiss();
-      this.setState({showEmojiInput: true});
+      this.setState({ showEmojiInput: true });
     }
   }
 
   _onFocus() {
     if (this.state.showEmojiInput)
-      this.setState({showEmojiInput: false});
+      this.setState({ showEmojiInput: false });
   }
 
   _onEmoticonPress(emoji) {
-    const { selection, textValue} = this.state;
-    const {start, end} = selection;
+    const { selection, textValue } = this.state;
+    const { start, end } = selection;
     let str = textValue.split('');
     str.splice(selection.end, 0, emoji);
     this.setState({
-      textValue: str.join(''), 
-      selection: {start:end+emoji.length, end:end+emoji.length },
+      textValue: str.join(''),
+      selection: { start: end + emoji.length, end: end + emoji.length },
     });
-    setTimeout(()=>{
+    setTimeout(() => {
       // force update selection
       this.textInput.setNativeProps({
-        selection: {start: end + emoji.length, end: end + emoji.length},
+        selection: { start: end + emoji.length, end: end + emoji.length },
       });
     }, 30);
   }
 
   _onBackspacePress() {
     const { selection, textValue } = this.state;
-    const {start, end} = selection;
+    const { start, end } = selection;
     const pres = spliddit(textValue.slice(0, selection.end));
-    const charlength = pres.length > 0 ? pres[pres.length - 1].length: 0;
+    const charlength = pres.length > 0 ? pres[pres.length - 1].length : 0;
     let str = textValue.split('');
-    str.splice(selection.end-charlength, charlength);
+    str.splice(selection.end - charlength, charlength);
     this.setState({
-      textValue: str.join(''), 
-      selection: {start: end-charlength, end: end-charlength}
+      textValue: str.join(''),
+      selection: { start: end - charlength, end: end - charlength }
     });
-    setTimeout(()=>{
+    setTimeout(() => {
       // force update selection
       this.textInput.setNativeProps({
-        selection: {start: end-charlength, end: end-charlength},
+        selection: { start: end - charlength, end: end - charlength },
       });
     }, 30);
   }
 
   _onSelectionChange(e) {
-    this.setState({selection: e.nativeEvent.selection});
+    this.setState({ selection: e.nativeEvent.selection });
   }
 
   render() {
-    const icon = this.state.showEmojiInput?'\ue608': "\ue892";
-    const color = this.state.showEmojiInput ? Theme.themeColor: '#888';
+    const icon = this.state.showEmojiInput ? '\ue608' : "\ue892";
+    const color = this.state.showEmojiInput ? Theme.themeColor : '#888';
     return (
-      <View style={{position: 'absolute', left: 0, bottom: 0, right: 0}}>
+      <View style={{ position: 'absolute', left: 0, bottom: 0, right: 0 }}>
         <View style={styles.container}>
           <TouchableWithoutFeedback onPress={this._onEmojiSwitchPress.bind(this)} >
-            <IconFont style={{padding: 6}} icon={icon} color={color} size={22} />
+            <IconFont style={{ padding: 6 }} icon={icon} color={color} size={22} />
           </TouchableWithoutFeedback>
           <View style={styles.textInputWrap} >
             <TextInput
               multiline={true}
               onChangeText={this._onChangeText.bind(this)}
-              onContentSizeChange={(event) => { 
-                this.setState({height: Math.min(80, event.nativeEvent.contentSize.height)})
+              onContentSizeChange={(event) => {
+                this.setState({ height: Math.min(80, event.nativeEvent.contentSize.height) })
               }}
-              placeholder={this.props.placeholder||"说点什么..."}
-              style={[styles.textInput, {height: Math.max(16, this.state.height)}]}
+              placeholder={this.props.placeholder || "说点什么..."}
+              style={[styles.textInput, { height: Math.max(16, this.state.height) }]}
               underlineColorAndroid="transparent"
               value={this.state.textValue}
               onSelectionChange={this._onSelectionChange.bind(this)}
-              ref={ref=>this.textInput=ref}
+              ref={ref => this.textInput = ref}
               selectionColor={Theme.themeColor}
               onFocus={this._onFocus.bind(this)}
               blurOnSubmit={false}
             />
           </View>
           <View style={styles.sendButtonWrap}>
-          {
-            this.state.send_ing ?
-            <ActivityIndicator size='small' color={Theme.themeColor} /> :
-            <Text onPress={this._onSendPress.bind(this)} style={{fontSize:15,
-              color: this.state.textValue!=''? '#111':'#888'}}>发送</Text>
-          }
+            {
+              this.state.send_ing ?
+                <ActivityIndicator size='small' color={Theme.themeColor} /> :
+                <Text onPress={this._onSendPress.bind(this)} style={{
+                  fontSize: 15,
+                  color: this.state.textValue != '' ? '#111' : '#888'
+                }}>发送</Text>
+            }
           </View>
         </View>
         <Emoticons
@@ -150,35 +152,35 @@ export default class BottomInputBar extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    borderTopWidth: 0.5, 
+    borderTopWidth: 0.5,
     elevation: 3,
-    borderColor:'#ddd', 
-    backgroundColor:'rgba(255,255,255,.95)', 
-    flexDirection:'row',
-    alignItems:'flex-end', 
-    padding: 8, 
-    paddingTop:0, 
-    paddingBottom:4, 
-    minHeight:48,
+    borderColor: '#ddd',
+    backgroundColor: 'rgba(255,255,255,.95)',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    padding: 8,
+    paddingTop: 0,
+    paddingBottom: 4,
+    minHeight: 48,
   },
   textInputWrap: {
-    flex:1, 
-    flexDirection:'row', 
-    alignItems:'flex-end', 
-    margin:4, marginTop:2, 
-    paddingBottom:2, 
-    borderBottomWidth:1, 
-    borderColor:'#d4d4d4',
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    margin: 4, marginTop: 2,
+    paddingBottom: 2,
+    borderBottomWidth: 1,
+    borderColor: '#d4d4d4',
   },
   textInput: {
-    padding:0, 
-    flex:1, 
-    fontSize:15,
+    padding: 0,
+    flex: 1,
+    fontSize: 15,
   },
   sendButtonWrap: {
-    width:40, 
-    paddingLeft:4, 
-    paddingBottom:8, 
-    justifyContent:'center',
+    width: 40,
+    paddingLeft: 4,
+    paddingBottom: 8,
+    justifyContent: 'center',
   },
 });
